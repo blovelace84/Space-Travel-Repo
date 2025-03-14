8b11f20edb0841424dcdb112e3dcaaa925546ecd
Brittani Lovelace <blovelace84@gmail.com>
2025-03-14 09:03:42 -0400

    Hardcoding supabaseUrl and anon key

--- Space Travel/src/supabaseClient.jsx
+++ Space Travel/src/supabaseClient.jsx
@@ -1,8 +1,8 @@
 // frontend/src/supabaseClient.js
 
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL; // Get from environment variables
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY; // Get from environment variables
+console.log(import.meta.env);
+const supabaseUrl = 'https://bhzlvzlupxxexmedcqpy.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoemx2emx1cHh4ZXhtZWRjcXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5MDg5MDYsImV4cCI6MjA1NzQ4NDkwNn0.QbBnv-LLdais3QsX4SEtjhr0s_2NijPo43xHu4XKDss'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
