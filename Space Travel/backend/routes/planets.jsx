d51b3d9b1bc72eaee120866a8191eda9695f1607
Brittani Lovelace <blovelace84@gmail.com>
2025-03-15 08:41:41 -0400

    Delete

--- Space Travel/backend/routes/planets.jsx
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const planetsController = require('../controllers/planetsController');
-
-router.get('/', planetsController.getPlanets); // Get / Planets
-
-module.exports = router;
--- Space Travel/src/supabaseClient.jsx
+++
@@ -1,8 +0,0 @@
-// frontend/src/supabaseClient.js
-
-import { createClient } from '@supabase/supabase-js';
-console.log(import.meta.env);
-const supabaseUrl = 'https://bhzlvzlupxxexmedcqpy.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoemx2emx1cHh4ZXhtZWRjcXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5MDg5MDYsImV4cCI6MjA1NzQ4NDkwNn0.QbBnv-LLdais3QsX4SEtjhr0s_2NijPo43xHu4XKDss'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
